3dbd946a19426d9811c1dbc08dde9c7b9a4058b3
github username <github email address>
2019-07-30 17:12:14 -0400

    made minor changes to game card

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", setUpPage)
-const GAMES_URL = `http://localhost:3000/api/v1/games/`
-
-function setUpPage(){
-    fetch(GAMES_URL)
-    .then(res => res.json())
-    .then(console.log)
-}
---
+++ js/index.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", setUpPage)
+const GAMES_URL = `http://localhost:3000/api/v1/games/`
+const LIKES_URL = `http://localhost:3000/api/v1/likes`
+const COMMENTS_URL = `http://localhost:3000/api/v1/comments`
+document.addEventListener("click", handleClick)
+
+function setUpPage(){
+    fetch(GAMES_URL)
+    .then(res => res.json())
+    .then(games => games.map(game => gameCard(game)))
+}
+
+function gameCard(game){
+    let gameList = document.querySelector("#list")
+
+    gameList.innerHTML += `
+        <li class="game-title" data-id=${game.id}>${game.title}</li>
+    `
+}
+
+function handleClick(e){
+    if (e.target.className == "game-title"){
+       let ID = e.target.dataset.id
+       fetch(GAMES_URL + ID)
+       .then(res => res.json())
+       .then(game => showMore(game))
+    }
+}
+
+function showMore(game){
+    console.log("game", game)
+    console.log("comments", game.comments)
+    console.log("likes", game.likes)
+    let showPanel = document.querySelector("#show-panel")
+    showPanel.innerHTML = ""
+    showPanel.innerHTML = `
+    <div data-game-id="${game.id}" class="card" style="width: 18rem;">
+    <img src=${game.image} class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${game.title}</h5>
+      <p class="card-text">Genre: ${game.genre}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Release Date: ${game.release_date}</li>
+      <li class="list-group-item">Dapibus ac facilisis in</li>
+      <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+    
+    `
+}
+
+
+
+    
+
+
+
