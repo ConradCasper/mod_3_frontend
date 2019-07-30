277ad4e8aae1b4f998af73bab741b38a9b3ffd34
github username <github email address>
2019-07-30 17:13:31 -0400

    added css file and template for new game form

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 document.addEventListener("DOMContentLoaded", setUpPage)
 const GAMES_URL = `http://localhost:3000/api/v1/games/`
+const addBtn = document.getElementById("new-game-btn")
+const formContainer = document.getElementById('form-container')
+let addGame = false
 
 function setUpPage(){
     fetch(GAMES_URL)
     .then(res => res.json())
     .then(console.log)
 }
+
+addBtn.addEventListener('click', () => {
+    // hide & seek with the form
+    addGame = !addGame
+    if (addGame) {
+      formContainer.style.display = 'block'
+      // submit listener here
+    } else {
+      formContainer.style.display = 'none'
+    }
+  })
+  
