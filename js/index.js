f062f06dd1a3651760ac509246ad8a54e4f23fad
github username <github email address>
2019-07-31 17:11:52 -0400

    likes and comments are updating on the DOM

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const formContainer = document.getElementById('form-container')
 let addGame = false
 const addGameForm = document.getElementById("add-game-form")
 addGameForm.addEventListener("submit", handleSubmit)
+let commentSubmit = document.querySelector("#commentSubmit")
 
 function setUpPage(){
     fetch(GAMES_URL)
@@ -37,6 +38,33 @@ function handleClick(e){
             method: "DELETE"
         })
         
+    } else if (e.target.id == "likeBtn"){
+       let game_Id = parseInt(e.target.dataset.id)
+
+       let oldLikes = parseInt(document.querySelector("#likes").innerText)
+       let newLikes = oldLikes + 1
+       document.querySelector("#likes").innerText = newLikes
+
+       let body = {
+           game_id: game_Id
+       }
+
+       
+
+        fetch(LIKES_URL, {
+            method: "POST",
+            headers: {
+                "content-type":"application/json",
+                accept:"application/json"
+            },
+            body:JSON.stringify({
+                like: body
+            })
+
+        })
+        .then(res => res.json())
+        .then(json => console.log(json))
+
     }
 }
 
@@ -44,23 +72,34 @@ function showMore(game){
     let id = game.id
     
     let showPanel = document.querySelector("#show-panel")
-    showPanel.innerHTML = ""
+
     showPanel.innerHTML = `
     <div data-game-id="${game.id}" class="card" style="width: 18rem;">
         <img src=${game.image} class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${game.title}</h5>
-            <p class="card-text">Genre: ${game.genre}</p>
-            <p class="card-text">Release Year: ${game.release_date}</p>
-            <p class ="card-text">Likes: ${game.likes.length}</p>
+            <p class="card-text"><h5>Genre:</h5> ${game.genre}</p>
+            <p class="card-text"><h5>Release Year:</h5> ${game.release_date}</p>
+            <p class="card-text"><h5>Description:</h5> ${game.description}</p>
+            <p class ="card-text">
+                <h5>Likes: 
+                    <span id="likes">${game.likes.length}</span>
+                </h5>
+                <button id="likeBtn" data-id="${game.id}" type="button" class="btn btn-outline-info">Like</button> 
+                
+            </p>
             <h6 class="card-text">Comments:</h6>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Create New Comment</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
+            <form>
+                <div class="form-group">
+                    <label for="commentTextArea">Create New Comment</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <button id="commentSubmit" type="submit">Submit Comment</button>
+                </div>
+            </form>
         </div>
         <ul id="comment-list" class="list-group list-group-flush">
-            ${game.comments.map(comment => `<li class="list-group-item" 
+            ${game.comments.map(comment => `
+            <li class="list-group-item" 
             id="comment-id-${comment.id}">${comment.content}
             <button class = "deleteCmntBtn" data-id = "${comment.id}">Delete</button>
             </li>`).join("")}
@@ -82,7 +121,6 @@ addBtn.addEventListener('click', () => {
 
 function handleSubmit(e) {
   e.preventDefault()
-  debugger;
   let newGame = {
     title: e.target.title.value,
     image: e.target.image.value,
